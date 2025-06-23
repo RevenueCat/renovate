9bc66e233f254dc6169287e3f4bc2bad9e08b597
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-06-23 14:46:23 +0200

    Adds Renovate workflow and initial config.

---
+++ .github/renovate-bot-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  branchPrefix: 'renovate/',
+  onboarding: true,
+  platform: 'github',
+  dryRun: 'full',
+  repositories: [
+    'RevenueCat/purchases-flutter',
+  ],
+  packageRules: [
+    
+  ],
+};
