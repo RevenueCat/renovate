27f813d65dd440c8615dbb7054953cbeedb86011
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-06-23 15:30:02 +0200

    Extracts some common config to default.json.

--- .github/renovate-bot-config.js
+++ .github/renovate-bot-config.js
@@ -1,24 +1,16 @@
 module.exports = {
   branchPrefix: 'renovate/',
-  onboarding: false,
+  onboarding: true,
   platform: 'github',
   dryRun: 'full',
   repositories: [
     'RevenueCat/renovate',
     'RevenueCat/purchases-flutter',
   ],
-  // Move these to a preset.
-  dependencyDashboard: true,
   packageRules: [
-    // Disable everything except updates to Gradle itself.
     {
       matchPackageNames: ['*'],
-      enabledManagers: ['gradle-wrapper'],
-    },
-    // Enable Android Gradle Plugin updates.
-    {
-      matchPackageNames: ['com.android.tools.build:gradle'],
-      enabledManagers: ['gradle'],
+      enabledManagers: ['github-actions'],
     },
   ],
 };
