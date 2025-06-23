c6abe1d4350999e56d3ec4d3b87974ce9aacbd97
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-06-23 15:01:35 +0200

    Adjusts the packageRules.

--- .github/renovate-bot-config.js
+++ .github/renovate-bot-config.js
@@ -7,6 +7,15 @@ module.exports = {
     'RevenueCat/purchases-flutter',
   ],
   packageRules: [
-    
+    // Disable everything except updates to Gradle itself.
+    {
+      matchPackageNames: ['*'],
+      enabledManagers: ['gradle-wrapper'],
+    },
+    // Enable Android Gradle Plugin updates.
+    {
+      matchPackageNames: ['com.android.tools.build:gradle'],
+      enabledManagers: ['gradle'],
+    },
   ],
 };
